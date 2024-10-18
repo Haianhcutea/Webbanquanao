47051beafba503b10d6e9b746e49008f4aa09f73
linh <linhntph43230@fpt.edu.vn>
2024-10-18 13:35:16 +0700

    tesst git

--- server/models/product.js
+++ server/models/product.js
@@ -23,6 +23,8 @@ const productSchema = new mongoose.Schema({
   updated_at: { type: Date, default: Date.now }
 });
 
+const abc ="abc";
+
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
