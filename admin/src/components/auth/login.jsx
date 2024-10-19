be037369f14fece6675f7e4856581d4c9bee880d
linh <linhntph43230@fpt.edu.vn>
2024-10-19 20:17:43 +0700

    linh commit

--- admin/src/components/auth/login.jsx
+++ admin/src/components/auth/login.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { openNotificationWithIcon, NotificationContext } from "../../App";
 import { useDispatch } from "react-redux";
-import { setUser, setLoading } from "../../store/auth-slice";
+import { setUser, setLoading } from "../../store/auth";
 
 const Login = () => {
   const api = useContext(NotificationContext);
@@ -16,10 +16,7 @@ const Login = () => {
     dispatch(setLoading(true));
 
     try {
-      const response = await axios.post(
-        `http://localhost:5555/api/auth/login`,
-        values
-      );
+      const response = await axios.post(`http://localhost:5555/api/auth/login`, values);
 
       if (response.status === 200) {
         const userData = response.data;
@@ -28,59 +25,25 @@ const Login = () => {
         localStorage.setItem("user", JSON.stringify(userData));
         localStorage.setItem("token", userData?.token);
 
-        openNotificationWithIcon(
-          api,
-          "success",
-          "Login Successful",
-          "You have successfully logged in!"
-        );
+        openNotificationWithIcon(api, "success", "Login Successful", "You have successfully logged in!");
         navigate("/");
       }
     } catch (error) {
-      openNotificationWithIcon(
-        api,
-        "error",
-        "Login Failed",
-        "Please check your credentials and try again."
-      );
+      openNotificationWithIcon(api, "error", "Login Failed", "Please check your credentials and try again.");
     } finally {
       dispatch(setLoading(false));
     }
   };
 
   return (
-    <Row
-      style={{
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
+    <Row style={{ alignItems: "center", justifyContent: "center", height: "100vh" }}>
       <Col span={8}>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: "Please input your Email!" }]}
-          >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-            />
+        <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
+          <Form.Item name="email" rules={[{ required: true, message: "Please input your Email!" }]}>
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
           </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
+          <Form.Item name="password" rules={[{ required: true, message: "Please input your Password!" }]}>
+            <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
           </Form.Item>
           <Form.Item>
             <Form.Item name="remember" valuePropName="checked" noStyle>
@@ -91,11 +54,7 @@ const Login = () => {
             </a>
           </Form.Item>
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
+            <Button type="primary" htmlType="submit" className="login-form-button">
               Log in
             </Button>
             Or <Link to="/register">register now!</Link>
