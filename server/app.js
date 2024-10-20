2b5b265f91bcf221002f8840a1a2bca4f7d1f184
linh <linhntph43230@fpt.edu.vn>
2024-10-20 19:27:57 +0700

    linh sua config

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: 'http://127.0.0.1:5173',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: [
       'Content-Type',
