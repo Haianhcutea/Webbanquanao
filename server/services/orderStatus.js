a10bc998bd16a27f1e59dccab68e8506bd4e66f5
Haianhcutea <anhnhph36303@fpt.edu.vn>
2025-01-15 22:41:30 +0700

    seerver update trạng thái đơn hàng

--- server/services/orderStatus.js
+++ server/services/orderStatus.js
@@ -2,13 +2,12 @@ const getOrderStatuses = () => {
     return [
       { id: 1, name: "Chưa xác nhận", englishName: "Pending" },
       { id: 2, name: "Đã xác nhận", englishName: "Confirmed" },
-      { id: 3, name: "Đã thanh toán", englishName: "Paid" },
-      { id: 4, name: "Chuẩn bị hàng", englishName: "Preparing" },
-      { id: 5, name: "Đang giao", englishName: "Transit" },
-      { id: 6, name: "Đã giao", englishName: "Delivered" },
-      { id: 7, name: "Đã nhận", englishName: "Received" },
-      { id: 8, name: "Đơn hàng thành công", englishName: "Completed" },
-      { id: 9, name: "Hủy đơn hàng", englishName: "Canceled" },
+      { id: 3, name: "Chuẩn bị hàng", englishName: "Preparing" },
+      { id: 4, name: "Đang giao", englishName: "Transit" },
+      { id: 5, name: "Đã giao", englishName: "Delivered" },
+      { id: 6, name: "Đã nhận", englishName: "Received" },
+      { id: 7, name: "Đơn hàng thành công", englishName: "Completed" },
+      { id: 8, name: "Hủy đơn hàng", englishName: "Canceled" },
     ];
   };
   
